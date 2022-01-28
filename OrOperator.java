45031e14de8aa8fd2129d886c18673be67a45eaa
kumarrcpm <kumarmtechdcn@gmail.com>
2022-01-28 09:12:29 +0530

    OROperator

--- OrOperator.java
+++ OrOperator.java
@@ -5,7 +5,7 @@ public class OrOperator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int a=5,b=5,c=3,d=10;
-if (a==b || d>c)
+if (a==b || c>d)
 {
 	System.out.println("OR Operator is working");
 }
