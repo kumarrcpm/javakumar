feeaf5aeb0e0b038dfbd57c67abd590eac3b0086
kumarrcpm <kumarmtechdcn@gmail.com>
2024-05-18 13:18:13 +0530

    Schduler

---
+++ Scheduler/src/main/java/com/example/demo/controller/MyObject.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import java.util.Date;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class MyObject implements Job {
+
+	@Override
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+		// TODO Auto-generated method stub
+System.out.println("Scheduler is on" + new Date());
+	}
+
+}
